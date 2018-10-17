7512a1bdd00b617038f0a6391fabb43423afa9b4
Robert Baxter <roer.baxter@gmail.com>
2018-10-17 11:53:46 -0400

    heroku deployment

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
-const Sequelize=require('sequelize')
+const Sequelize = require("sequelize");
 
-const sequelize=new Sequelize(process.env.NAME,'postgres',process.env.PW,{
-    host:'localhost',
-    dialect:'postgres'
-})
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres"
+});
 
-sequelize.authenticate().then(
-    ()=>console.log('Connected to instructional app database'),
-    ()=>console.log(err)
-)
+sequelize
+  .authenticate()
+  .then(
+    () => console.log("Connected to instructional app database"),
+    () => console.log(err)
+  );
 
-module.exports=sequelize
+module.exports = sequelize;
