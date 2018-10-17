d2aa45fb4c9303436c037abbe7e3e842a417a46a
Robert Baxter <roer.baxter@gmail.com>
2018-10-17 14:32:48 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ sequelize.sync(); // {force:true} to reset
 
 app.use(bodyParser.json()); // returns middleware and only looks at requests where the 'Content-Type' header matches 'type' option
 app.use(require("./middleware/headers")); //Middleware for authentication
-app.use(require("./middleware/validate-session"));
 
 //Exposed routes (authentication subject to individual routes)
 app.use("/api/user", user);
 
 //Protected routes (always requires authentication)
+app.use(require("./middleware/validate-session"));
 app.use("/api/submission", submission);
 app.use("/api/assignment", assignment);
 
