acccf0a6288fce220fedac3dce0785f3b5a270d6
Robert Baxter <roer.baxter@gmail.com>
2018-10-19 13:26:00 -0400

    db toggle

--- db.js
+++ db.js
@@ -1,13 +1,19 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-  "postgresql://postgres:" +
-    encodeURIComponent(process.env.PW) +
-    "@localhost/instructionalapp",
-  {
-    dialect: "postgres"
-  }
-);
+//Use this for development
+// const sequelize = new Sequelize(
+//   "postgresql://postgres:" +
+//     encodeURIComponent(process.env.PW) +
+//     "@localhost/instructionalapp",
+//   {
+//     dialect: "postgres"
+//   }
+// );
+
+//Use this for deployment
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres"
+});
 
 sequelize
   .authenticate()
